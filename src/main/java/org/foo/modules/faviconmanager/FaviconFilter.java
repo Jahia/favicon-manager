242b82e2c8df6bcfeb628f14848d5a180acfde31
Florent BOURASSE <fbourasse@jahia.com>
2026-06-22 20:21:18 +0200

    test: add first FaviconFilter tests, tidy imports + coverage wiring
    - Add JUnit 4 + Mockito unit tests for FaviconFilter (favicon routing,
      jmix:favicon lookup, no-favicon case, and the chain-always-proceeds
      robustness guarantee when site resolution throws).
    - Replace the javax.servlet.* wildcard import with explicit imports.
    - Wire JaCoCo coverage for SonarQube reporting; add CHANGELOG.
    
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/main/java/org/foo/modules/faviconmanager/FaviconFilter.java
+++ src/main/java/org/foo/modules/faviconmanager/FaviconFilter.java
@@ -9,7 +9,11 @@ import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
